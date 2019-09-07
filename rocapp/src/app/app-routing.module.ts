08d82d229e3e179ae8645323019e509e0ba0951b
Lautaro <garcia.a.lautaro@gmail.com>
2019-09-06 21:50:31 -0300

    Mejorar la responsividad de la rocapp
    Quité muchos de los estilos choripasteados por estilos custom usando más
    flexbox y unidades relativas

--- rocapp/src/app/app-routing.module.ts
+++ rocapp/src/app/app-routing.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {APP_BASE_HREF} from '@angular/common';
 import {EmptyRouteComponent} from './empty-route/empty-route.component';
-import {AppComponent} from './app.component';
 
 
-const routes: Routes = [{ path: '**', component: EmptyRouteComponent }]
+const routes: Routes = [{ path: '**', component: EmptyRouteComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
